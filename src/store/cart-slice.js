6c2a3af8e4f65068fbeb4a88c5f4e47a1983c340
yevgen-sydoruk <61704998+yevgen-sydoruk@users.noreply.github.com>
2023-06-01 19:55:42 -0500

    Adding react-redux toolkit with ui and cart slices

---
+++ src/store/cart-slice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  items: [],
+  totalQuantity: 0,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: initialState,
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      if (!existingItem) {
+        state.items.push({
+          itemId: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        });
+      } else {
+        existingItem.quantity = existingItem.quantity + 1;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+      }
+    },
+    removeItemFromCart(state) {},
+  },
+});
+
+export const cartActions = cartSlice.actions;
+export default cartSlice.reducer;
